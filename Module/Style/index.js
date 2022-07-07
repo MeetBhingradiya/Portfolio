24c821c2d15bd92caf1dc6f5eed07391443c8697
MeetBhingradiya <meetbhingradiya36@gmail.com>
2022-07-07 18:26:36 +0530

    UPDATE 0.2 Bug Test 3

--- Module/Style/index.js
+++ Module/Style/index.js
@@ -1,57 +1,14 @@
-/*===== MENU SHOW =====*/ 
-const showMenu = (toggleId, navId) =>{
-    const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId)
+function ScrollEffect()
+    {
+        const navbar = document.getElementById('navigationbar');
+        const navbarclass = document.getElementById('navigationbar').classList
+        const TotalHeight = document.body.scrollHeight - window.innerHeight;
+        window.onscroll = function NavbarEffect()
+            {
+                const ScrolledHeight = window.pageYOffset;
+                const ScrolledDown = ScrolledHeight - 0;
+                const ScrolledTop = ScrolledDown - ScrolledHeight;
 
-    if(toggle && nav){
-        toggle.addEventListener('click', ()=>{
-            nav.classList.toggle('show')
-        })
+                if(ScrolledDown =)
+            }
     }
-}
-showMenu('nav-toggle','nav-menu')
-
-/*===== ACTIVE AND REMOVE MENU =====*/
-const navLink = document.querySelectorAll('.nav__link');   
-
-function linkAction(){
-  /*Active link*/
-  navLink.forEach(n => n.classList.remove('active'));
-  this.classList.add('active');
-  
-  /*Remove menu mobile*/
-  const navMenu = document.getElementById('nav-menu')
-  navMenu.classList.remove('show')
-}
-navLink.forEach(n => n.addEventListener('click', linkAction));
-
-/*===== SCROLL REVEAL ANIMATION =====*/
-const sr = ScrollReveal({
-    origin: 'top',
-    distance: '80px',
-    duration: 2000,
-    reset: true
-});
-
-/*SCROLL HOME*/
-sr.reveal('.home__title',{}); 
-sr.reveal('.button',{delay: 200}); 
-sr.reveal('.home__img',{delay: 200}); 
-sr.reveal('.home__social-icon',{ interval: 200}); 
-
-/*SCROLL ABOUT*/
-sr.reveal('.about__img',{}); 
-sr.reveal('.about__subtitle',{delay: 200}); 
-sr.reveal('.about__text',{delay: 200}); 
-
-/*SCROLL SKILLS*/
-sr.reveal('.skills__subtitle',{}); 
-sr.reveal('.skills__text',{}); 
-sr.reveal('.skills__data',{interval: 200}); 
-sr.reveal('.skills__img',{delay: 200});
-
-/*SCROLL WORK*/
-sr.reveal('.work__img',{interval: 200}); 
-
-/*SCROLL CONTACT*/
-sr.reveal('.contact__input',{interval: 200}); 

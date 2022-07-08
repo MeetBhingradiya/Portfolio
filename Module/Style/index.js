176a4ec073db27699ae06fada7ac8022fcb8f326
MeetBhingradiya <meetbhingradiya36@gmail.com>
2022-07-08 10:33:19 +0530

    VS UPDATE Fecher Bug Test 1

--- Module/Style/index.js
+++ Module/Style/index.js
@@ -1,3 +1,4 @@
+const Links = document.querySelectorAll('.li')
 function ScrollEffect()
     {
         const navbar = document.getElementById('navigationbar');
@@ -9,6 +10,25 @@ function ScrollEffect()
                 const ScrolledDown = ScrolledHeight - 0;
                 const ScrolledTop = ScrolledDown - ScrolledHeight;
 
-                if(ScrolledDown =)
+                // if(ScrolledDown =)
             }
     }
+function Activator(i)
+    {
+        for(Link of Links)
+        Link.classList.remove('active');
+        Links[i].classList.add('active');
+    }
+for (let j = 0;j<Links.length;j++)
+    {
+        Links[j].addEventListener('click',()=>{
+            Activator(j);
+        })
+    }
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+});

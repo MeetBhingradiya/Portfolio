bac7b0c829e47c7915b8ec5370d0c42f13f50559
Meet Bhingradiya <meetbhingradiya36@gmail.com>
2024-08-31 13:55:37 +0530

    Coming Soon Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,21 +2,28 @@ import Image from "next/image";
 
 export default function Home() {
     return (
-        <div>
-            <h1>Meet Bhingradiya&apos;s Portfolio</h1>
-            <Image
-                src="/meet.jpg"
-                alt="Meet Bhingradiya"
-                width={200}
-                height={200}
-                property="false"
-            />
-            <p>
-                Coming Soon!
-            </p>
-            <p>
-                Blogs, ResearchPapers, Projects, Dev Tools and more...
-            </p>
-        </div>
+        <>
+            <div className="Container">
+                <h1>Meet Bhingradiya</h1>
+                <Image
+                    src="/meet.jpg"
+                    alt="Meet Bhingradiya"
+                    width={200}
+                    height={200}
+                    property="false"
+                />
+                <p className="soon">
+                    Coming Soon!
+                </p>
+                <p>
+                    Blogs, Projects, and more...
+                </p>
+            </div>
+            <div className="footer">
+                <a href="https://github.com/meetbhingradiya">
+                    2021 - 2024 © Meet Bhingradiya
+                </a>
+            </div>
+        </>
     );
 }

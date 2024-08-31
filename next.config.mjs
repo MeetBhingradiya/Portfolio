ae3f459143390ceab67ce5793d1f564a38c00d66
Meet Bhingradiya <meetbhingradiya36@gmail.com>
2024-08-31 16:30:49 +0530

    CSP & Anti IFrame Update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,39 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    webpack5: true,
+    images: {
+        domains: ['localhost'],
+    },
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'X-Frame-Options',
+                        value: 'DENY',
+                    },
+                    {
+                        key: 'X-Content-Type-Options',
+                        value: 'nosniff',
+                    },
+                    {
+                        key: 'X-XSS-Protection',
+                        value: '1; mode=block',
+                    },
+                    {
+                        key: 'Referrer-Policy',
+                        value: 'same-origin',
+                    },
+                    {
+                        key: 'Content-Security-Policy',
+                        value: `default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self' data:; connect-src 'self'; frame-ancestors 'none'; base-uri 'self'; form-action 'self';`,
+                    },
+                ],
+            },
+        ];
+    },
+};
 
 export default nextConfig;

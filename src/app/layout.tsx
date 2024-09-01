1cb7d7dc0908437e7ed3e417c6329b9b9ff66848
Meet Bhingradiya <meetbhingradiya36@gmail.com>
2024-09-01 08:45:29 +0530

    SEO Fix for increase rank of the site from 5 to more up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,13 +6,22 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
     title: "Meet Bhingradiya",
-    description: "A personal website for Meet Bhingradiya.",
+    description: "A Portfolio Website of Meet Bhingradiya",
+    icons: "favicon.ico",
     keywords: [
         "Meet Bhingradiya", 
         "Meet", 
         "Bhingradiya",
         "Portfolio",
-        "Meet Bhingradiya Portfolio"
+        "Meet Bhingradiya Portfolio",
+        "meetbhingradiya",
+        "Full Stack Developer",
+        "Full Stack",
+        "Surat, Gujarat",
+        "Gujarat, India",
+        "Surat",
+        "Gujarat",
+        "India",
     ],
 };
 
@@ -27,6 +36,7 @@ export default function RootLayout({
                 <meta charSet="utf-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <meta name="theme-color" content="#000" />
+
 
                 {/* Google Search Console Verification */}
                 <meta name="google-site-verification" content="-eIAp0-BRCYjfoSuMDWpQTpgjQHadfvBbnf4le5IWBk" />

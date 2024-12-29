8b0e84d34e69e40d6fa148c5bd7d462d015721b1
Meet Bhingradiya <meetbhingradiya36@gmail.com>
2024-12-29 15:40:07 +0530

    🧪 PreRelease v1.0.5 Candidate 3

--- src/Config/index.ts
+++ src/Config/index.ts
@@ -1,6 +1,6 @@
 const Config = {
     version: "1.0.5",
-    releasedate: "2025-1-1",
+    releasedate: "2024-12-29",
     visiblebranch: "Development",
     isHomeReleased: true,
     Environment: process.env.NODE_ENV,

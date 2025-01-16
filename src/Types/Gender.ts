3b1b2de4fdc3398e5d2ec5c448250809b5abf894
Meet Bhingradiya <meetbhingradiya36@gmail.com>
2025-01-16 12:52:06 +0530

    Gender Enum

---
+++ src/Types/Gender.ts
@@ -0,0 +1,28 @@
+/**
+ *  @FileID          Types\Gender.ts
+ *  @Description     Currently, there is no description available.
+ *  @Author          @MeetBhingradiya
+ *  
+ *  -----------------------------------------------------------------------------
+ *  Copyright (c) 2025 Meet Bhingradiya
+ *  All rights reserved.
+ *  
+ *  This file is part of the @MeetBhingradiya's Portfolio project and is protected under copyright
+ *  law. Unauthorized copying of this file, via any medium, is strictly prohibited
+ *  without explicit permission from the author.
+ *  
+ *  -----------------------------------------------------------------------------
+ *  @created 16/01/25 12:51 PM IST (Kolkata +5:30 UTC)
+ *  @modified 16/01/25 12:51 PM IST (Kolkata +5:30 UTC)
+ */
+
+enum IGender {
+    FEMALE = "Female",
+    MALE = "male",
+    CUSTOM = "Custom",
+    UNSPECIFIED = "Unspecified"
+}
+
+export {
+    IGender
+}
